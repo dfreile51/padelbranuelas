2b312498b20cabafde5a690b5e5266b411cb067e
Diego Freile Garcia <dfreile51@gmail.com>
2023-05-25 09:21:24 +0200

    Cambios en el css del sidebar del Panel Admin

--- public/js/panelAdmin/sidebar.js
+++ public/js/panelAdmin/sidebar.js
@@ -1,21 +1,17 @@
 const toggleBtnIcon = document.querySelector('#interface .navigation .toggle-btn .material-symbols-outlined');
 const toggleBtn = document.querySelector('#interface .navigation .toggle-btn');
 const menu = document.querySelector('#menu');
-const interface = document.querySelector('#interface');
 const sombra = document.querySelector('#sombra');
 const sidebarCloseBtn = document.querySelector('#menu .closebtn');
 
 toggleBtn.addEventListener('click', () => {
     menu.setAttribute('style', 'width: 30rem');
-    interface.setAttribute('style', 'margin-left: 30rem');
     document.getElementsByTagName('body')[0].classList.add('no-scroll');
     sombra.classList.remove('hidden');
-    // toggleBtnIcon.textContent = 'close';
 });
 
 sidebarCloseBtn.addEventListener('click', () => {
     menu.removeAttribute('style');
-    interface.removeAttribute('style');
     document.getElementsByTagName('body')[0].classList.remove('no-scroll');
     sombra.classList.add('hidden');
 });
